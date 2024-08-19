a349dc8a35374a47c17c621517255356c8b3a00d
ben <bmfboom9@gmail.com>
2024-08-19 12:54:15 -0600

    game now pauses when you level up

--- unity projects/ShootyTower_MovingCastle/Assets/_Scripts/System/UIController_System.cs
+++ unity projects/ShootyTower_MovingCastle/Assets/_Scripts/System/UIController_System.cs
@@ -2,12 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Pause_System))]
+
 public class UIController_System : MonoBehaviour
 {
     public static UIController_System Instance;
 
     [Header("DEBUG")]
     [SerializeField] GameObject LevelUpMenu;
+    Pause_System pauseScript;
+
+    private void Awake()
+    {
+        pauseScript = gameObject.GetComponent<Pause_System>();
+    }
 
     private void Start()
     {
@@ -29,10 +37,12 @@ public class UIController_System : MonoBehaviour
     public void ShowLevelUpMenu()
     {
         LevelUpMenu.SetActive(true);
+        pauseScript.PauseGame();
     }
 
     public void HideLevelUpMenu()
     {
         LevelUpMenu.SetActive(false);
+        pauseScript.ResumeGame();
     }
 }
